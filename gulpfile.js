9c4b4d8f4da49d71b67fe07b67e22da9751d79fe
tunglinh10a2 <linhdautung@gmail.com>
2016-07-26 15:54:35 +0700

    add bower, debugbar and collective

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,22 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var files = {
+    'jquery': './public/bower/jquery2/jquery.min.js',
+    'jqueryBootstrap': './public/bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    'jqueryUI': './public/bower/jquery-ui/jquery-ui.min.js',
+    'jqueryUI_image': './public/bower/jquery-ui/themes/base/images/**',
+    'font_awesome': './public/bower/font-awesome/fonts/**',
+}
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .scripts([
+            files.jquery,
+            files.jqueryBootstrap,
+            files.jqueryUI,
+            'app.js',
+        ], 'public/js/app.js')
+        .copy(files.jqueryUI_image, 'public/css/images')
+        .copy(files.font_awesome, 'public/fonts');
 });
